e2acaf44553d15b91367776d29a98728170e1e9c
Fahri Izzuddin Zulkarnaen <fahrizulkarnaen1@gmail.com>
2023-11-27 20:59:47 +0700

    update crud indexeddb

--- src/components/shared/ContentCard.vue
+++ src/components/shared/ContentCard.vue
@@ -1,26 +1,34 @@
 <script setup>
 defineProps({
-  username: String,
-  imageUrl:String
-})
+   item: Object,
+   imageUrl: String,
+});
+
+const blobToImageUrl = (event) => {
+   return URL.createObjectURL(event);
+};
 </script>
 
 <template>
- <div class="card-container">
-    <img :src="imageUrl" style="width: 100px; height: auto;"/>
-    <span class="title-case-style">{{ username }}</span>
- </div>
+   <div class="card-container">
+      <div v-if="imageUrl !== null">
+         <img :src="blobToImageUrl(imageUrl)" style="width: 100px; height: auto" />
+      </div>
+      <span class="title-case-style">{{ item.id }}</span>
+      <span class="title-case-style">{{ item.title }}</span>
+      <p class="title-case-style" v-html="item.description"></p>
+   </div>
 </template>
 
 <style scoped>
- .card-container{
-    display: flex;
-    align-items: center;
+.card-container {
+   display: flex;
+   align-items: center;
 }
-.title-case-style{
-  font-size: 18px;
-  color: black;
-  margin-left: 10px;
+.title-case-style {
+   font-size: 18px;
+   color: black;
+   margin-left: 10px;
 }
 /* .card-container:hover{
   background-color: wheat;
